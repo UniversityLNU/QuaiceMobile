140e303b51b71f42b72dacdb35e67d3adeb46d1c
orest-paliy <paliy.o.2004@gmail.com>
2024-04-13 22:44:29 +0300

    fundAct

--- app/src/main/java/com/quaice/hackathonapp/MainActivity.java
+++ app/src/main/java/com/quaice/hackathonapp/MainActivity.java
@@ -4,10 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.quaice.hackathonapp.service.FundraisingService;
 
 public class MainActivity extends AppCompatActivity {
+    private FundraisingService fundraisingService;
     //fundraising
+    private RelativeLayout fundLayout;
     private RecyclerView fundraisingRecyclerView;
+
+    public void showFundraising(){
+        fundLayout.setVisibility(View.VISIBLE);
+        fundraisingService = new FundraisingService(this);
+
+        //getAllfund
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
